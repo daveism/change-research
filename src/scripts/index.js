a5df474e10a4def0532470be8c154e1a543e2dcc
Dave Michelson <daveism@gmail.com>
2020-01-15 22:50:20 -0500

    this is pretty hacky but it works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,7 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import mapboxgl from 'mapbox-gl';
-import MapboxDraw from '@mapbox/mapbox-gl-draw';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import { Store } from './store';
-import RadiusMode from './radiusMode';
-import drawStyles from './drawstyles';
 import { GoogleAnalytics } from './ga';
 
 const store = new Store({});
@@ -43,7 +39,7 @@ googleAnalytics.setEvent('data', 'mobile', isMobileDevice());
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2ZWlzbSIsImEiOiJCdjUxT0FzIn0.V9oIk_wUc4uZu7UBblR8mw';
 
 const map = new mapboxgl.Map({
-  container: 'map',
+  container: 'map-1',
   style: 'mapbox://styles/mapbox/streets-v11',
   // 'mapbox://styles/daveism/cjwrrdfd20uic1dnzsti2owlk', - dark
   center: [-98, 38.88], // starting position [lng, lat]
@@ -53,39 +49,8 @@ const map = new mapboxgl.Map({
   keybindings: true
 });
 
-
-// setup map
-const drawControl = new MapboxDraw({
-  displayControlsDefault: true,
-  controls: {
-    rectangle: true,
-    polygon: true,
-    line_string: true,
-    trash: true
-  },
-  options: {
-    touchEnabled: true,
-    keybindings: true,
-    touchBuffer: 10
-  },
-  styles: drawStyles,
-  modes: Object.assign({
-    draw_radius: RadiusMode
-  }, MapboxDraw.modes)
-});
-
-map.addControl(drawControl);
-
 const nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
-
-const geocoder = new MapboxGeocoder({
-  accessToken: mapboxgl.accessToken,
-  mapboxgl,
-  setZoom: 8,
-  flyTo: false,
-  placeholder: 'Search for a location...'
-});
 
 map.on('zoomend', () => {
   console.log('searchzoom', map.getZoom())
@@ -105,20 +70,24 @@ map.on('zoomend', () => {
 
 // function
 function handleAgreeClick() {
-  document.getElementById('study-progress').classList.remove('d-none');
+  const studyVersion = 1; //Math.floor(Math.random() * (3 - 1 + 1) + 1);
+
+  document.getElementById(`study-progress-map-${studyVersion}`).classList.remove('d-none');
   document.getElementById('study-agreement-all').classList.add('d-none');
-  document.getElementById('study-dissaggree').remove();
-  store.setStateItem('study-agreement', true);
-  // document.getElementById('map-action-holder').classList.remove('h-80');
-  // document.getElementById('map-action-holder').classList.add('h-70');
 
-  document.getElementById('map-action-holder').classList.remove('start-height-actions');
-  document.getElementById('map-holder').classList.remove('start-height-map');
-
-  document.getElementById('map-action-holder').classList.add('step-height-actions');
-  document.getElementById('map-holder').classList.add('step-height-map');
+  // document.getElementById('study-dissaggree').remove();
+  // document.getElementById('map-holder').classList.remove('d-none');
+  // document.getElementById('study-agreement-all').classList.add('d-none');
+  // document.getElementById('study-dissaggree').remove();
+  // document.getElementById('map-holder').classList.remove('start-height-map');
+  // // document.getElementById('map-action-holder').classList.remove('start-height-actions');
+  // document.getElementById('map-action-holder').classList.add('step-height-actions');
+  // document.getElementById('map-action-holder').classList.add('col-sm-12');
+  // document.getElementById('map-action-holder').classList.add('col-md-5');
+  // document.getElementById('map-action-holder').classList.remove('col-xl-5');
+  // document.getElementById('map-holder').classList.add('step-height-map');
   map.resize();
-  // ga event action, category, label
+  store.setStateItem('study-agreement', true);
   googleAnalytics.setEvent('data', 'study-agreement', true);
   return null;
 }
@@ -147,38 +116,6 @@ function checkValidObject(obj) {
 
 function uuid() {
   return crypto.getRandomValues(new Uint32Array(4)).join('-');
-}
-
-function handleDrawButtonClick(e) {
-  const circleButtonElem = document.getElementById(`${e.target.id}`);
-  if (circleButtonElem) {
-    if (circleButtonElem.classList.contains('disabled')) {
-      $(`#${e.target.id}`).tooltip({ trigger: 'hover focus' });
-      $(`#${e.target.id}`).tooltip('show');
-      return null;
-    } else { // eslint-disable-line
-      $(`#${e.target.id}`).tooltip({ trigger: 'manual' });
-      $(`#${e.target.id}`).tooltip('hide');
-      $(`#${e.target.id}`).tooltip('disable');
-      $(`#${e.target.id}`).tooltip('dispose');
-    }
-  }
-
-  drawControl.trash();
-
-  if (map.getLayer('circle-line')) {
-    map.removeLayer('circle-line');
-  }
-
-  if (map.getLayer('circle-fill')) {
-    map.removeLayer('circle-fill');
-  }
-  if (map.getSource('circle')) {
-    map.removeSource('circle');
-  }
-
-  drawControl.changeMode('draw_radius');
-  return null;
 }
 
 // check study session state for completetion
@@ -230,208 +167,123 @@ if (studyCompleted) { // || studyAgrreed
   store.setStateItem('studycompleted', false);
 }
 
-geocoder.on('result', (e) => {
-  const x = e.result.center[0];
-  const y = e.result.center[1];
+// function handleStepNavClick(e) {
+//   const valNode = e.target.getAttributeNode('val'); // eslint-disable-line
+//   if (valNode) {
+//
+//     document.getElementById('step-1').classList.remove('step-not-vis');
+//     document.getElementById('step-2').classList.remove('step-not-vis');
+//     document.getElementById('step-3').classList.remove('step-not-vis');
+//     document.getElementById('step-1').classList.add('step-not-vis');
+//     document.getElementById('step-2').classList.add('step-not-vis');
+//     document.getElementById('step-3').classList.add('step-not-vis');
+//     const value = e.target.getAttributeNode('val').value; // eslint-disable-line
+//     document.getElementById(`${value}`).classList.remove('step-not-vis');
+//   }
+// }
+//
+// // const stepNav1Elem = document.getElementById('step-nav-1');
+// if (stepNav1Elem) {
+//   stepNav1Elem.addEventListener('click', handleStepNavClick);
+// }
+//
+// const stepNav2Elem = document.getElementById('step-nav-2');
+// if (stepNav2Elem) {
+//   stepNav2Elem.addEventListener('click', handleStepNavClick);
+// }
+//
+// const stepNav3Elem = document.getElementById('step-nav-3');
+// if (stepNav3Elem) {
+//   stepNav3Elem.addEventListener('click', handleStepNavClick);
+// }
+//
+// function handleSubmitButtonClick(e) {
+//   const submitButtonElem = document.getElementById('submit-button');
+//   if (submitButtonElem.classList.contains('disabled')) {
+//     $('#submit-button').tooltip({ trigger: 'hover focus' });
+//     $('#submit-button').tooltip('show');
+//     return null;
+//   } else { // eslint-disable-line
+//     $('#submit-button').tooltip({ trigger: 'manual' });
+//     $('#submit-button').tooltip('hide');
+//     $('#submit-button').tooltip('disable');
+//     $('#submit-button').tooltip('dispose');
+//
+//     const circle = store.getStateItem('circle');
+//     const line = store.getStateItem('line');
+//     const distancekm = store.getStateItem('distancekm');
+//     const distancemeters = store.getStateItem('distancemeters');
+//     const distancefeet = store.getStateItem('distancefeet');
+//     const distancemiles = store.getStateItem('distancemiles');
+//     const studydistancequestion = store.getStateItem('studydistancequestion');
+//
+//     // ga event action, category, label
+//     googleAnalytics.setEvent('data', 'circle-submitted', circle);
+//     googleAnalytics.setEvent('data', 'line-submitted', line);
+//     googleAnalytics.setEvent('data', 'distancekm-submitted', distancekm);
+//     googleAnalytics.setEvent('data', 'distancefeet-submitted', distancefeet);
+//     googleAnalytics.setEvent('data', 'distancemeters-submitted', distancemeters);
+//     googleAnalytics.setEvent('data', 'distancemiles-submitted', distancemiles);
+//
+//     document.getElementById('study-complete-question').innerHTML = `${studydistancequestion}`;
+//     document.getElementById('study-complete-miles').innerHTML = `${distancemiles.toFixed(2)} miles or`;
+//     document.getElementById('study-complete-feet').innerHTML = `${distancefeet.toFixed(2)} feet or`;
+//     document.getElementById('study-complete-km').innerHTML = `${distancekm.toFixed(2)} kilometers or`;
+//     document.getElementById('study-complete-meters').innerHTML = `${distancemeters.toFixed(2)} meters.`;
+//
+//     // end study
+//     document.getElementById('study-complete').classList.remove('d-none');
+//     document.getElementById('study-progress').remove();
+//     document.getElementById('map-holder').remove();
+//     document.getElementById('study-agreement-all').remove();
+//     document.getElementById('map-action-holder').className ='col-12'; // eslint-disable-line
+//     store.setStateItem('studycompleted', true);
+//
+//     // ga event action, category, label
+//     googleAnalytics.setEvent('data', 'studycompleted', true);
+//   }
+//   return null;
+// }
+//
+// const submitButtonElem = document.getElementById('submit-button');
+// if (submitButtonElem) {
+//   submitButtonElem.addEventListener('click', handleSubmitButtonClick);
+// }
 
-  // ga event action, category, label
-  googleAnalytics.setEvent('data', 'searchpoint', `${x}, ${y}`);
+// const directionsOne = [
+//   'Search for a location you care about.',
+//   'Search for a location to find about crime.',
+//   'Search for a location to find about the closest pizza place.'
+// ];
+//
+// const minOne = 0;
+// const maxOne = 2;
+// const messageIndexOne = Math.floor(Math.random() * (maxOne - minOne + 1) + minOne);
+// const stepDirections1 = document.getElementById('step1-directions');
+// if (stepDirections1) {
+//   stepDirections1.innerHTML = directionsOne[messageIndexOne];
+// }
+//
+// // ga event action, category, label
+// googleAnalytics.setEvent('data', 'step1text', directionsOne[messageIndexOne]);
 
-  const offsetdist = 0.0025;
-  const bbox = [[x - offsetdist, y - offsetdist], [x + offsetdist, y + offsetdist]];
+// const directionsTwo = [
+//   'Draw a circle that represents 1 mile.',
+//   'Draw a circle that represents a 5-minute <strong>DRIVE</strong>.',
+//   'Draw a circle that represents a 5-minute <strong>WALK</strong>.'
+// ];
 
-  // create random zoom incase users are influenced by intial zoomlevel
-  let min = 10;
-  let max = 14;
-  if (isMobileDevice()) {
-    min = 10;
-    max = 15;
-  }
-
-  const zm = Math.floor(Math.random() * (max - min + 1) + min);
-  map.fitBounds(bbox, { maxZoom: zm });
-
-  // ga event action, category, label
-  googleAnalytics.setEvent('data', 'searchzoom', zm);
-  console.log('searchzoom', zm)
-
-  const circleButtonElem = document.getElementById('circle-button');
-  if (circleButtonElem.classList.contains('disabled')) {
-    circleButtonElem.classList.remove('disabled');
-    $('#circle-button').tooltip({ trigger: 'manual' });
-    $('#circle-button').tooltip('hide');
-    $('#circle-button').tooltip('disable');
-    $('#circle-button').tooltip('dispose');
-    document.getElementById('step2-title').classList.remove('disabled');
-    document.getElementById('step2-directions').classList.remove('disabled');
-
-    document.getElementById('step-1').classList.add('step-not-vis');
-    document.getElementById('step-2').classList.remove('step-not-vis');
-  }
-});
-
-const geocodeElem = document.getElementById('geocoder');
-if (geocodeElem) {
-  geocodeElem.appendChild(geocoder.onAdd(map));
-
-  geocodeElem.addEventListener('touchstart', (e) => {
-    // e.preventDefault();
-    geocodeElem.classList.remove('expand');
-    geocodeElem.classList.add('expand');
-  });
-}
-
-const suggestionsElem = document.querySelector('#geocoder .suggestions-wrapper');
-if (suggestionsElem) {
-  suggestionsElem.addEventListener('touchstart', (e) => {
-    const geocodeElem = document.getElementById('geocoder'); // eslint-disable-line
-    geocodeElem.classList.remove('expand');
-  });
-}
-
-const drawCircleElement = document.querySelector('.btn-draw-circle');
-if (drawCircleElement) {
-  drawCircleElement.addEventListener('click', handleDrawButtonClick);
-}
-
-const reDrawCircleElement = document.querySelector('.btn-redraw-circle');
-if (reDrawCircleElement) {
-  reDrawCircleElement.addEventListener('click', handleDrawButtonClick);
-}
-
-function handleStepNavClick(e) {
-  const valNode = e.target.getAttributeNode('val'); // eslint-disable-line
-  if (valNode) {
-    const geocodeElem = document.getElementById('geocoder'); // eslint-disable-line
-    if (geocodeElem) {
-      geocodeElem.classList.remove('expand');
-    }
-
-    document.getElementById('step-1').classList.remove('step-not-vis');
-    document.getElementById('step-2').classList.remove('step-not-vis');
-    document.getElementById('step-3').classList.remove('step-not-vis');
-    document.getElementById('step-1').classList.add('step-not-vis');
-    document.getElementById('step-2').classList.add('step-not-vis');
-    document.getElementById('step-3').classList.add('step-not-vis');
-    const value = e.target.getAttributeNode('val').value; // eslint-disable-line
-    document.getElementById(`${value}`).classList.remove('step-not-vis');
-  }
-}
-
-const stepNav1Elem = document.getElementById('step-nav-1');
-if (stepNav1Elem) {
-  stepNav1Elem.addEventListener('click', handleStepNavClick);
-}
-
-const mainContentElem = document.getElementById('main-content'); // eslint-disable-line
-if (mainContentElem) {
-  mainContentElem.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('mapboxgl-ctrl-geocoder--input')) {
-      const geocodeElem = document.getElementById('geocoder'); // eslint-disable-line
-      if (geocodeElem) {
-        geocodeElem.classList.remove('expand');
-      }
-    }
-  });
-}
-
-const stepNav2Elem = document.getElementById('step-nav-2');
-if (stepNav2Elem) {
-  stepNav2Elem.addEventListener('click', handleStepNavClick);
-}
-
-const stepNav3Elem = document.getElementById('step-nav-3');
-if (stepNav3Elem) {
-  stepNav3Elem.addEventListener('click', handleStepNavClick);
-}
-
-function handleSubmitButtonClick(e) {
-  const submitButtonElem = document.getElementById('submit-button');
-  if (submitButtonElem.classList.contains('disabled')) {
-    $('#submit-button').tooltip({ trigger: 'hover focus' });
-    $('#submit-button').tooltip('show');
-    return null;
-  } else { // eslint-disable-line
-    $('#submit-button').tooltip({ trigger: 'manual' });
-    $('#submit-button').tooltip('hide');
-    $('#submit-button').tooltip('disable');
-    $('#submit-button').tooltip('dispose');
-
-    const circle = store.getStateItem('circle');
-    const line = store.getStateItem('line');
-    const distancekm = store.getStateItem('distancekm');
-    const distancemeters = store.getStateItem('distancemeters');
-    const distancefeet = store.getStateItem('distancefeet');
-    const distancemiles = store.getStateItem('distancemiles');
-    const studydistancequestion = store.getStateItem('studydistancequestion');
-
-    // ga event action, category, label
-    googleAnalytics.setEvent('data', 'circle-submitted', circle);
-    googleAnalytics.setEvent('data', 'line-submitted', line);
-    googleAnalytics.setEvent('data', 'distancekm-submitted', distancekm);
-    googleAnalytics.setEvent('data', 'distancefeet-submitted', distancefeet);
-    googleAnalytics.setEvent('data', 'distancemeters-submitted', distancemeters);
-    googleAnalytics.setEvent('data', 'distancemiles-submitted', distancemiles);
-
-    document.getElementById('study-complete-question').innerHTML = `${studydistancequestion}`;
-    document.getElementById('study-complete-miles').innerHTML = `${distancemiles.toFixed(2)} miles or`;
-    document.getElementById('study-complete-feet').innerHTML = `${distancefeet.toFixed(2)} feet or`;
-    document.getElementById('study-complete-km').innerHTML = `${distancekm.toFixed(2)} kilometers or`;
-    document.getElementById('study-complete-meters').innerHTML = `${distancemeters.toFixed(2)} meters.`;
-
-    // end study
-    document.getElementById('study-complete').classList.remove('d-none');
-    document.getElementById('study-progress').remove();
-    document.getElementById('map-holder').remove();
-    document.getElementById('study-agreement-all').remove();
-    document.getElementById('map-action-holder').className ='col-12'; // eslint-disable-line
-    store.setStateItem('studycompleted', true);
-
-    // ga event action, category, label
-    googleAnalytics.setEvent('data', 'studycompleted', true);
-  }
-  return null;
-}
-
-const submitButtonElem = document.getElementById('submit-button');
-if (submitButtonElem) {
-  submitButtonElem.addEventListener('click', handleSubmitButtonClick);
-}
-
-const directionsOne = [
-  'Search for a location you care about.',
-  'Search for a location to find about crime.',
-  'Search for a location to find about the closest pizza place.'
-];
-
-const minOne = 0;
-const maxOne = 2;
-const messageIndexOne = Math.floor(Math.random() * (maxOne - minOne + 1) + minOne);
-const stepDirections1 = document.getElementById('step1-directions');
-if (stepDirections1) {
-  stepDirections1.innerHTML = directionsOne[messageIndexOne];
-}
-
-// ga event action, category, label
-googleAnalytics.setEvent('data', 'step1text', directionsOne[messageIndexOne]);
-
-const directionsTwo = [
-  'Draw a circle that represents 1 mile.',
-  'Draw a circle that represents a 5-minute <strong>DRIVE</strong>.',
-  'Draw a circle that represents a 5-minute <strong>WALK</strong>.'
-];
-
-const minTwo = 0;
-const maxTwo = 2;
-const messageIndexTwo = Math.floor(Math.random() * (maxTwo - minTwo + 1) + minTwo);
-const stepDirections2 = document.getElementById('step2-directions');
-if (stepDirections2) {
-  stepDirections2.innerHTML = directionsTwo[messageIndexTwo];
-}
-
-// ga event action, category, label
-googleAnalytics.setEvent('data', 'step2text', directionsTwo[messageIndexTwo]);
-store.setStateItem('studydistancequestion', directionsTwo[messageIndexTwo]);
+// const minTwo = 0;
+// const maxTwo = 2;
+// const messageIndexTwo = Math.floor(Math.random() * (maxTwo - minTwo + 1) + minTwo);
+// const stepDirections2 = document.getElementById('step2-directions');
+// if (stepDirections2) {
+//   stepDirections2.innerHTML = directionsTwo[messageIndexTwo];
+// }
+//
+// // ga event action, category, label
+// googleAnalytics.setEvent('data', 'step2text', directionsTwo[messageIndexTwo]);
+// store.setStateItem('studydistancequestion', directionsTwo[messageIndexTwo]);
 
 const aggreeButtonElement = document.getElementById('aggree-button');
 if (aggreeButtonElement) {
@@ -443,9 +295,9 @@ if (dissaggreeButtonElement) {
   dissaggreeButtonElement.addEventListener('click', handleDissagreeClick);
 }
 
-const step2MinorDirectionsElement = document.getElementById('step2-minor-directions');
-if (step2MinorDirectionsElement) {
-  if (isMobileDevice()) {
-    step2MinorDirectionsElement.innerHTML = 'Click on the "Draw a circle" button. Click on the map and drag your finger across the map until the radius of the circle best represents the distance.';
-  }
-}
+// const step2MinorDirectionsElement = document.getElementById('step2-minor-directions');
+// if (step2MinorDirectionsElement) {
+//   if (isMobileDevice()) {
+//     step2MinorDirectionsElement.innerHTML = 'Click on the "Draw a circle" button. Click on the map and drag your finger across the map until the radius of the circle best represents the distance.';
+//   }
+// }
